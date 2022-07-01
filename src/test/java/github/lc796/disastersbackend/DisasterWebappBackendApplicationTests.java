8a5335647f9548e3cab5a91eeb91892a47cc0a7a
Luke Collishaw <lc796@exeter.ac.uk>
2022-07-01 11:25:45 +0100

    Initial commit

---
+++ src/test/java/github/lc796/disastersbackend/DisasterWebappBackendApplicationTests.java
@@ -0,0 +1,10 @@
+package github.lc796.disastersbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisasterWebappBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
